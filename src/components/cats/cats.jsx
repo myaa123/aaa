1b79ee3f9ca1c3a57d5e0b6bf4e36d0cff0cfcd8
Karishma Chadha <kchadha@media.mit.edu>
2019-03-27 11:01:28 -0400

    Some updates.

--- src/components/cats/cats.jsx
+++ src/components/cats/cats.jsx
@@ -6,7 +6,6 @@ const Modal = require('../modal/base/modal.jsx');
 require('./cats.scss');
 
 const catImages = [
-    '/images/cats/IMG_9798.jpg',
     '/images/cats/IMG_9775.jpg',
     '/images/cats/IMG_9766.jpg',
     '/images/cats/IMG_9587.jpg',
@@ -31,9 +30,14 @@ const catImages = [
     '/images/cats/IMG_1157.jpg',
     '/images/cats/IMG_0681.jpg',
     '/images/cats/IMG_0135.jpg',
-    '/images/cats/IMG_0071.jpg'
+    '/images/cats/IMG_0071.jpg',
+    '/images/cats/1.jpg',
+    '/images/cats/2.jpg',
+    '/images/cats/3.jpg',
+    '/images/cats/4.jpg'
 ];
 
+/* eslint-disable max-len */
 const catFacts = [
     'Cats don\'t have 9 lives, but they DO have 9 toes.',
     'All cats are turquoise for a few minutes after they\'re born.',
@@ -58,7 +62,6 @@ const catFacts = [
     'What even is a cat?',
     'The song "Row, Row, Row your boat" is about the authors pet cat.',
     'Cat\'s really like listening to music, but only if it is meowsic.',
-    'No.',
     'The first cat was born in 1993.',
     'The Great Catsby is often considered a favorite among cats.',
     'When you\'re not home cats will often try on your shoes.',
@@ -72,7 +75,6 @@ const catFacts = [
     'Kitty McPurrs-a-lot would make an excellent name for a cat.',
     'Cats blame mice for everything.',
     'Boots & cats & boots & cats & boots & cats & boots & cats.',
-    // eslint-disable-next-line max-len
     'When a cat is happy sometimes you can hear the soft whirr of its storage system recording the experience for an upcoming show & tell presentation.',
     'Some people think cats are aloof but actually they just really respect your independence.',
     'Modern cats evolved from a creature that looked exactly like Scratch Cat, but more orange.',
@@ -82,9 +84,54 @@ const catFacts = [
     'Kittens were invented by scientists developing a standard measuring system for "cute."',
     'Due to the difference between metric units and other systems, 1 "mew" is about 1.6 "mrow" units.',
     'Cats sometimes knock things off shelves and tables in order to make those items safe from falling.',
-    'A cat\'s tail is usually its second- or third-best friend.'
+    'A cat\'s tail is usually its second- or third-best friend.',
+    'Catalogs were originally invented to keep logs of cats.',
+    'Catering is food services for cats.',
+    'Meow is slang for miauw.',
+    'A cat is the only physical entity whose weight equals both 1 kilogram and 1 pound.',
+    'Balls of yarn are created by watching videos of cats playing with them in reverse.',
+    'Every time you sneeze a tiny kitten is born.',
+    'In ancient Greece, cats kept humans as pets.',
+    'Bongo cat is the great grandchild of nyancat.',
+    'What\'s a cat\'s best friend? Its purrrrse.',
+    'Sharing video online was created by cats to get the attention they feel they deserve.',
+    'A "catseye" view is a point of view where everything is scratchable.',
+    'Cat videos is the first step in their quest for world domination.',
+    'The cat\'s meow is the cat\'s meow is the cat\'s meow is the cat\'s meow....',
+    'A cat\'s intelligence is distributed throughout its body. Fully half of its brain matter is in its tail.',
+    'Cats evolved chromatophores millions of years before octopuses.',
+    'Cats are able to read minds through their whiskers.',
+    'Soon after birth, cats extend their tails into a breeze and can sail hundreds of feet in the air for miles to find new territory.',
+    'Cats\' memory is encoded in their DNA, which explains the knowing looks on their faces.',
+    'In winter, cats cacoon themselves onto the sides of maple trees, slowly licking the sap to survive.',
+    'Charles Francis Richter originally created his scale to measure the integrated power of cat purrs.',
+    'Cat hair is being studied for use in building space elevators. Ironically, the first use of space elevators will be as scratching posts.',
+    'When you are not looking, cats stare at a point exactly one foot behind your head.',
+    'The infamous Weddell Sea cats glide from tree to tree in the forests of Dronning Maud.',
+    'Once the antarctic ice melts, archologists believe the origins of cat civilization will be revealed.',
+    'Cats rest on their backs because it helps them to absorb cuteness through their bellies.',
+    'The 21cm hydrogen line was originally named after a 21cm long cat.',
+    'Redshift would be a good name for a cat that does not like you and runs away.',
+    'The MIT mascot is actually a cat that enjoys wood working.',
+    'The cat packing conjecture is a widely accepted mathematical argument that proves an infinite number of cats is a good thing.',
+    'Meor\'s Law states that the number of cats double every 18-24 months.',
+    'A group of cats is called a hairball.',
+    'There are more hairs on the head of a cat than there are scales on the head of a mouse.',
+    'April 32nd is International Pet a Cat Day.',
+    'The Scratch Cat is the only cat on the Internet.',
+    'This sentence about cats is true.',
+    'Cat bites are more common on days that end in y.',
+    'Cats have a minimum of two ears.',
+    'Cattails were developed in a lab as a way to reconstruct cats using plants. It didn’t work.',
+    'Cats developed certain fur patterns to blend in with couch upholstery.',
+    'Cats have thumbs, they’re just really really hard to see.',
+    'Cats are the first most popular pet in the US, after dogs.',
+    'If you rick roll a cat, it will never forgive you.',
+    'Cats persuaded humans to create the internet.',
+    'If you walk under a ladder and then pet a black cat, it will actually bring you good luck.'
 ];
 
+/* eslint-enable max-len */
 
 class Cats extends React.Component {
     constructor (props) {
